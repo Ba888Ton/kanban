e71f0251f8604fbd19ce910346a70ca3e6db7199
Anton Skorikov <antonskorikov@iMac-Evgenij.local>
2020-10-15 17:09:38 +0300

    add dnd example

--- src/kanban.js
+++ src/kanban.js
@@ -209,8 +209,30 @@ boardList[1].addEventListener(`dragend`, (evt) => {
 evt.target.classList.remove(`selected`);
 });
 
+// mainField.addEventListener(`dragover`, (evt) => {
+// 	// Разрешаем сбрасывать элементы в эту область
+// 	evt.preventDefault();
+// 	// Находим перемещаемый элемент
+// 	const activeElement = boardList[0].querySelector(`.selected`);
+// 	// Находим элемент, над которым в данный момент находится курсор
+// 	const currentElement = evt.target;
+// 	// Проверяем, что событие сработало:
+// 	// 1. не на том элементе, который мы перемещаем,
+// 	// 2. именно на элементе списка
+// 	const isMoveable = activeElement !== currentElement &&
+// 	  currentElement.classList.contains(`list`);
+// 	// Если нет, прерываем выполнение функции
+// 	if (!isMoveable) {
+// 	  return;
+// 	}
+// 	// Находим элемент, перед которым будем вставлять
+// 	const nextElement = boardList[1].lastChild.nextSibling;
+// 	// Вставляем activeElement перед nextElement
+// 	boardList[1].appendChild(activeElement);
+//   });
+
 boardList[1].addEventListener(`dragover`, (evt) => {
-	// Разрешаем сбрасывать элементы в эту область
+	//Разрешаем сбрасывать элементы в эту область
 	evt.preventDefault();
 	// Находим перемещаемый элемент
 	const activeElement = boardList[0].querySelector(`.selected`);
@@ -233,27 +255,53 @@ boardList[1].addEventListener(`dragover`, (evt) => {
 	boardList[1].insertBefore(activeElement, nextElement);
   });
 
-boardList[1].addEventListener(`dragover`, (evt) => {
-	// Разрешаем сбрасывать элементы в эту область
-	console.log('nado mnoj')
-	evt.preventDefault();
-	// // Находим перемещаемый элемент
-	// const activeElement = boardList[1].querySelector(`.selected`);
-	// // Находим элемент, над которым в данный момент находится курсор
-	// const currentElement = evt.target;
-	// // Проверяем, что событие сработало:
-	// // 1. не на том элементе, который мы перемещаем,
-	// // 2. именно на элементе списка
-	// const isMoveable = activeElement !== currentElement &&
-	//   currentElement.classList.contains(`list-item`);
-	// // Если нет, прерываем выполнение функции
-	// if (!isMoveable) {
-	//   return;
-	// }
-	// // Находим элемент, перед которым будем вставлять
-	// const nextElement = (currentElement === activeElement.nextElementSibling) ?
-	// 	currentElement.nextElementSibling :
-	// 	currentElement;
-	// // Вставляем activeElement перед nextElement
-	// boardList[1].insertBefore(activeElement, nextElement);
-  });
+
+
+
+
+// const empty = document.querySelectorAll(".empty");
+// const drag = document.querySelector(".drag");
+
+// drag.addEventListener("dragstart", dragStart);
+// drag.addEventListener("dragend", dragEnd);
+
+// // requestAnimationFrame - подменяет стиль на прозрачный. работает с частотой 60fps
+// function dragStart(event) {
+//   console.log("start");
+//   this.style.backgroundColor = "green";
+//   requestAnimationFrame(() => (this.style.backgroundColor = "transparent"), 0);
+// }
+
+// function dragEnd() {
+//   console.log("end");
+//   this.style.backgroundColor = "red";
+// }
+
+// for (const item of empty) {
+//   item.addEventListener("dragover", dragOver);
+//   item.addEventListener("dragenter", dragEnter);
+//   item.addEventListener("dragleave", dragLeave);
+//   item.addEventListener("drop", dragDrop);
+// }
+
+// // dragOver срабатывает всё время
+// // preventDefault - обязательно! Сбрасывает стандартные обработчики браузера
+// function dragOver() {
+//   event.preventDefault();
+// }
+
+// // dragEnter срабатывает над объектом один раз
+// function dragEnter() {
+//   this.style.backgroundColor = "gray";
+// }
+
+// // dragLeave срабатывает после ухода
+// function dragLeave() {
+//   this.style.backgroundColor = "black";
+// }
+
+// // drop срабатывает после того, как перетаскиваемый объект опустится на блок
+// function dragDrop() {
+//   console.log("drop");
+//   this.append(drag);
+// }
